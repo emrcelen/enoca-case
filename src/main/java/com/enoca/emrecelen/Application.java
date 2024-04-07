91c339c55ee6f0813410902ad4197db046e430b4
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-04-07 21:29:01 +0300

    initial setup of project files completed

---
+++ src/main/java/com/enoca/emrecelen/Application.java
@@ -0,0 +1,16 @@
+package com.enoca.emrecelen;
+
+import com.enoca.emrecelen.model.entity.Cart;
+import com.enoca.emrecelen.model.entity.Customer;
+import com.enoca.emrecelen.model.enumaration.Constants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
